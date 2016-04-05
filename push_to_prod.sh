7b7ce1ca1b6116493e7395e20f32beccef2c0494
Ethan Jennings <ethanhjennings@gmail.com>
2016-04-04 23:19:59 -0500

    Fixing bad-gatway error and updating helper scripts

--- push_to_prod.sh
+++ push_to_prod.sh
@@ -1,10 +1,8 @@
 DOCKERHUB_USER="hallofswe"
 APP_NAME="dudecarryme"
+MYSQL_ENV_FILE="mysql-prod.env"
 
-# exit on any error
-set -e
-
-eval SWE_CLUSTER_PATH=~/swe_cluster/docker.env
+eval SWE_CLUSTER_PATH=~/carina_creds/docker.env
 echo ${SWE_CLUSTER_PATH}
 
 if [ ! -f "${SWE_CLUSTER_PATH}" ]
@@ -13,7 +11,21 @@ then
     exit 1
 fi
 
+# stop on any errors
+set -e
+
 source ${SWE_CLUSTER_PATH}
+
+# check if mysql .env file exists
+if [ ! -f "${MYSQL_ENV_FILE}" ]
+then
+    echo "Enter MySQL credentials:"
+    read -p "MySQL Username: " mysql_username
+    read -s -p "MySQL Password: " mysql_password
+    echo "MYSQL_USER=${mysql_username}" >> mysql-prod.env
+    echo "MYSQL_PASSWORD=${mysql_password}" >> mysql-prod.env
+    echo "MYSQL_ROOT_PASSWORD=${mysql_password}" >> mysql-prod.env
+fi 
 
 # fix an issue with windows path for certificate file
 if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ];
@@ -21,23 +33,33 @@ then
     export DOCKER_CERT_PATH=$(echo $DOCKER_CERT_PATH | sed 's#\/##1' | sed 's#\/#:\/#1')
 fi
 
-#login to docker-up
-docker login
+if [[ $* != *-s* ]];
+then
+    #login to docker-hub
+    printf "\n\nEnter docker-hub credentials:\n"
+    docker login
 
-# build images and push to docker-hub
-docker build -t ${DOCKERHUB_USER}/${APP_NAME}_app app
-docker push ${DOCKERHUB_USER}/${APP_NAME}_app
-docker build -t ${DOCKERHUB_USER}/${APP_NAME}_lb lb
-docker push ${DOCKERHUB_USER}/${APP_NAME}_lb
+    # build images and push to docker-hub
+    docker build -t ${DOCKERHUB_USER}/${APP_NAME}_app app
+    docker push ${DOCKERHUB_USER}/${APP_NAME}_app
+    docker build -t ${DOCKERHUB_USER}/${APP_NAME}_lb lb
+    docker push ${DOCKERHUB_USER}/${APP_NAME}_lb
+    docker build -t ${DOCKERHUB_USER}/${APP_NAME}_db db
+    docker push ${DOCKERHUB_USER}/${APP_NAME}_db
+fi
 
-# stop and remove all old containers
-docker-compose stop
-yes | docker-compose rm
+# stop and remove all containers (except carina containers)
+echo "removing old containers:"
+docker stop $(docker ps -aq -f "name=app") || true
+docker stop $(docker ps -aq -f "name=lb") || true
+docker rm $(docker ps -aq -f="name=db") || true
+docker rm $(docker ps -aq -f="name=app") || true
+docker rm $(docker ps -aq -f="name=lb") || true
 
 # start up server
-docker-compose --file docker-compose-prod.yml up -d > dockerup.txt
+docker-compose --file docker-compose-prod.yml up -d
 
-#create database
+# initialize database
 docker-compose --file docker-compose-prod.yml run -d --rm --no-deps app python app.py create_db
 
 # print ip and port
--- run_local.sh
+++ run_local.sh
@@ -1,16 +1,30 @@
+MYSQL_ENV_FILE="mysql-prod.env"
+
 # create local virtual machine if it doesn't exist
 docker-machine create --driver virtualbox default
 docker-machine start default
 
 # stop on any errors after this
-#set -e
+set -e
 
 # setup environment
 eval "$(docker-machine env default)"
 
+# check if mysql .env file exists
+if [ ! -f "${MYSQL_ENV_FILE}" ]
+then
+    echo "Enter MySQL credentials:"
+    read -p "MySQL Username: " mysql_username
+    read -s -p "MySQL Password: " mysql_password
+    echo "MYSQL_USER=${mysql_username}" >> mysql-prod.env
+    echo "MYSQL_PASSWORD=${mysql_password}" >> mysql-prod.env
+    echo "MYSQL_ROOT_PASSWORD=${mysql_password}" >> mysql-prod.env
+fi 
+
 # stop already running containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+printf "\nStopping old containers\n"
+docker stop $(docker ps -a -q) || true
+docker rm $(docker ps -a -q) || true
 
 # print ip
 printf "\nRunning at this ip: "
