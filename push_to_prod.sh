d608d767c0bd7299038936c7d739cb370858870f
Ethan Jennings <ethanhjennings@gmail.com>
2016-04-19 22:39:08 -0500

    Fixing push_to_prod.sh and adding source_prod.sh

--- push_to_prod.sh
+++ push_to_prod.sh
@@ -4,17 +4,10 @@ DOCKERHUB_USER="hallofswe"
 APP_NAME="dudecarryme"
 MYSQL_ENV_FILE="mysql-prod.env"
 
-eval SWE_CLUSTER_PATH=~/carina_creds/docker.env
-echo ${SWE_CLUSTER_PATH}
-
-if [ ! -f "${SWE_CLUSTER_PATH}" ]
-then
-    echo "No cluster file found"
-    exit 1
-fi
-
 # stop on any errors
 set -e
+
+source source_prod.sh
 
 if [[ $* != *-s* ]];
 then
@@ -31,16 +24,6 @@ then
     docker push ${DOCKERHUB_USER}/${APP_NAME}_db
 fi
 
-source ${SWE_CLUSTER_PATH}
-
-export DOCKER_HOST="${DOCKER_HOST%:2376}:42376"
-
-# fix an issue with windows path for certificate file
-if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ];
-then
-    export DOCKER_CERT_PATH=$(echo $DOCKER_CERT_PATH | sed 's#\/##1' | sed 's#\/#:\/#1')
-fi
-
 # check if mysql .env file exists
 if [ ! -f "${MYSQL_ENV_FILE}" ]
 then
@@ -54,18 +37,10 @@ fi
 
 
 
-echo "Stopping old containers:"
-docker-compose --file docker-compose-prod.yml down || true
-docker stop $(docker ps -a -q) || true
-#docker stop $(docker ps -aq -f "name=app") || true
-#docker stop $(docker ps -aq -f "name=lb") || true
-#docker stop $(docker ps -aq -f "name=db") || true
-#docker rm $(docker ps -aq -f="name=db") || true
-#docker rm $(docker ps -aq -f="name=app") || true
-#docker rm $(docker ps -aq -f="name=lb") || true
-#docker rmi $(docker images -aq -f="name=db") || true
-#docker rmi $(docker images -aq -f="name=app") || true
-#docker rmi $(docker images -aq -f="name=lb") || true
+echo "Stopping and deleting old containers:"
+docker stop $(docker ps -a) || true
+docker rm $(docker ps -a -q) || true
+docker rmi $(docker images -q) || true
 
 # start up server
 docker-compose --file docker-compose-prod.yml up -d
